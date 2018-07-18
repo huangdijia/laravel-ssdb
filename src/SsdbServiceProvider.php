fda59bac1493dd1d9f2808158772fceb0f2f0b05
huangdijia <huangdijia@gmail.com>
2018-07-19 07:45:00 +0800

    support use as session manager

--- src/SsdbServiceProvider.php
+++ src/SsdbServiceProvider.php
@@ -3,18 +3,34 @@
 namespace Huangdijia\Ssdb;
 
 use Huangdijia\Ssdb\Cache\Ssdb;
+use Huangdijia\Ssdb\Session\SsdbSessionHandler;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\ServiceProvider;
 
 class SsdbServiceProvider extends ServiceProvider
 {
+    /**
+     * 标记着提供器是延迟加载的
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
     public function boot()
     {
         Cache::extend('ssdb', function ($app) {
             return Cache::repository(new Ssdb($app));
         });
+        Session::extend('ssdb', function ($app) {
+            return new SsdbSessionHandler($app);
+        });
     }
 
+    /**
+     * 注册服务提供者
+     *
+     * @return void
+     */
     public function register()
     {
         $this->app->singleton('ssdb.simple', function () {
@@ -24,5 +40,15 @@ class SsdbServiceProvider extends ServiceProvider
                 config('cache.stores.ssdb.timeout', 2000)
             );
         });
+    }
+
+    /**
+     * 取得提供者提供的服务
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['ssdb.simple'];
     }
 }
