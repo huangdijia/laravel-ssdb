e6006b565eb05730176bf919fa055b60e7d78a45
huangdijia <huangdijia@gmail.com>
2019-09-27 16:49:11 +0800

    ci

---
+++ tests/SsdbTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Huangdijia\Ssdb\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class SsdbTest extends TestCase
+{
+    public function test_connect()
+    {
+        $this->assertIsBool(true);
+    }
+}
