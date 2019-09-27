9eb374f791819ae20b8e438849135687bb919513
huangdijia <huangdijia@gmail.com>
2019-09-27 17:19:19 +0800

    test

--- tests/SsdbTest.php
+++ tests/SsdbTest.php
@@ -7,32 +7,38 @@ use PHPUnit\Framework\TestCase;
 
 class SsdbTest extends TestCase
 {
-    protected $connection;
-
-    public function __construct()
+    public static function connection()
     {
-        $this->connection = new Simple('127.0.0.1', 8888, 2000);
+        static $connection = null;
+
+        if (is_null($connection) || $connection->closed()) {
+            $connection = new Simple('127.0.0.1', 8888, 2000);
+        }
+
+        return $connection;
     }
+
     public function test_single_set_and_get()
     {
         $key   = 'test';
         $value = time();
 
-        $this->connection->set($key, $value);
+        self::connection()->set($key, $value);
 
-        $cached = $this->connection->get($key);
+        $cached = self::connection()->get($key);
 
         $this->assertEquals($cached, $value);
+        $this->assertTrue(true);
     }
 
     public function test_multi_set_and_get()
     {
-        $this->connection->multi_set([
+        self::connection()->multi_set([
             'a' => 1,
             'b' => 2,
         ]);
 
-        $cached = $this->connection->multi_get(['a', 'b']);
+        $cached = self::connection()->multi_get(['a', 'b']);
 
         $this->assertIsArray($cached);
         $this->assertEquals(count($cached), 2);
