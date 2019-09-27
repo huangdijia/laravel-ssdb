d0d5691b090f5e6cacd2327cf0a7f528281284ab
huangdijia <huangdijia@gmail.com>
2019-09-27 16:57:31 +0800

    test

--- tests/SsdbTest.php
+++ tests/SsdbTest.php
@@ -2,12 +2,21 @@
 
 namespace Huangdijia\Ssdb\Tests;
 
+use Huangdijia\Ssdb\Simple;
 use PHPUnit\Framework\TestCase;
 
 class SsdbTest extends TestCase
 {
     public function test_connect()
     {
-        $this->assertIsBool(true);
+        $ssdb  = new Simple('127.0.0.1', 8888, 2000);
+        $key   = 'test';
+        $value = time();
+
+        $ssdb->set($key, $value);
+
+        $cached = $ssdb->get($key);
+
+        $this->assertEquals($cached, $value);
     }
 }
